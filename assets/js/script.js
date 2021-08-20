b2f28948246310223e86d3a866418e4e5fb2e817
kenzinielsen <kenzinielsen@Kenzis-MacBook-Air.local>
2021-08-19 22:04:56 -0600

    css buttons and q's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,44 @@
 var quizOpen = document.querySelector (".quizOpen");
 var start = document.getElementById ("start");
 var questionEl = document.querySelector(".questionEl");
-var index = 0
+var index = 0;
 var answers = document.querySelector(".answers");
-var time = 60
+var time = 60;
 var timerId;
 var timerEl = document.querySelector(".timerEl");
+
 var questions = [
     {
-        question: "question1", 
-        answers: ["answer1", "answer2", "answer3", "answer4" ],
+        question: "Which one is the OR operator?", 
+        answers: ["&&", "||", "===", "==" ],
         correct: "answer2",
     },
     {
-        question: "question2", 
-        answers: ["answer1", "answer2", "answer3", "answer4" ],
+        question: "Which of the following variables is visible everywhere in your javascript code?", 
+        answers: ["Global Variable", "Local Variable", "Both ", "None of the above" ],
+        correct: "answer1",
+    },
+    {
+        question: "Which is the correct way to write a function?", 
+        answers: ["function= 1", "function beginQuiz( ) { }", "beginQuiz( ).function", "None of the above" ],
         correct: "answer2",
     },
     {
-        question: "question3", 
-        answers: ["answer1", "answer2", "answer3", "answer4" ],
-        correct: "answer2",
-    },
-    {
-        question: "question4", 
-        answers: ["answer1", "answer2", "answer3", "answer4" ],
-        correct: "answer2",
+        question: "What does [ ] do in javascript?", 
+        answers: ["Creates an empty object", "Can be used to create a function", "Creates an empty array", "All of the above" ],
+        correct: "answer3",
     },
 ]
 
 function endQuiz() {
-    clearInterval(timerId)
+    clearInterval(timerId);
     questionEl.setAttribute("class", "disappear");
     answers.setAttribute("class", "disappear");
 }
 
 function startQuiz() {
-    quizOpen.setAttribute("class", "disappear")
-    timerId=setInterval(timer, 1000)
+    quizOpen.setAttribute("class", "disappear");
+    timerId=setInterval(timer, 1000);
     timerEl.textContent=time;
     getQuestion()
 }
@@ -52,17 +53,17 @@ function getQuestion() {
     questionEl.textContent=currentQuestion.question
     answers.innerHTML=""
     for (let i = 0; i < currentQuestion.answers.length; i++) {
-        var button = document.createElement("button")
-        button.textContent=currentQuestion.answers[i]
-        button.setAttribute("value", currentQuestion.answers[i])
+        var button = document.createElement("button");
+        button.textContent=currentQuestion.answers[i];
+        button.setAttribute("value", currentQuestion.answers[i]);
         button.onclick=getAnswer;
-        answers.append(button)
+        answers.append(button);
     }
 }
 
 function getAnswer() {
     if (this.value !== questions[index].correct) {
-        time-=5
+        time-=10;
     }
     index++;
     if (index===questions.length) {
@@ -71,7 +72,10 @@ function getAnswer() {
     } else {
         getQuestion();
     }
+}
 
+
+function highScores() {
 }
 
 start.addEventListener("click", startQuiz);
